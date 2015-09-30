20409d635880b18e40de562938be1cae4481b815
David AW Barton <david.barton@bristol.ac.uk>
2015-09-30 16:15:16 +0100

    Nice working version!

--- zebrafish_demo.py
+++ zebrafish_demo.py
@@ -18,12 +18,12 @@ from math import pi, cos, sin, tan, exp, asin, acos, atan, log
 # Path handling
 import os.path
 
+# Enable or disable debugging
+DEBUG = False
+
 # IPython for debugging
-try:
+if DEBUG:
     import IPython
-    has_ipython = True
-except Exception as e:
-    has_ipython = False
 
     
 # Class for the fish
@@ -32,17 +32,13 @@ class Fish(object):
 
     default_dt = 1/20
 
-    pars_epj = {"mu_u": 10, "sigma_u": 1.4*3, "sigma_omega": 2.85,
-                "sigma_0": 12, "theta_u": 0.52, "theta_omega": 4.21,
-                "K_s": 2.5, "K_p": 3, "K_v": 12, "r_u": 1.4*3,
-                "r_omega": 0.8*3, "d_c": 10, "delta": 10*3,
-                "f_W_a": 5.26, "f_W_b": 0.13}             
-
-    pars_school = {"mu_u": 10, "sigma_u": 2, "sigma_omega": 0,
-                   "sigma_0": 12, "theta_u": 0.52, "theta_omega": 3,
-                   "K_s": 2, "K_p": 5, "K_v": 12, "r_u": 3.6,
-                   "r_omega": 1.8, "d_c": 45, "delta": 30,
-                   "f_W_a": 5.26, "f_W_b": 0.13}             
+    default_pars = {"mu_u": 10, "sigma_u": 4.2,
+                    "theta_u": 0.52, "theta_omega": 4.21,
+                    "K_s": 2.5, "K_p": 3, "K_v": 12, "r_u": 4.2,
+                    "r_omega": 2.4, "d_c": 30, "delta": 30,
+                    "f_W_a": 5.26, "f_W_b": 0.13,
+                    "f_c_a": 5, "f_c_b": 0.2,
+                    "U_min": 1e-4, "U_max": 30, "Omega_max": 10}
     
     def __init__(self):
         self.ndim = 5
@@ -50,9 +46,9 @@ class Fish(object):
         # Initialise the state with no fish
         self.state = np.zeros((0, self.ndim))
         # Parameters for the simulation
-        self.tank_radius = 100
+        self.tank_radius = 50
         self.dt =  Fish.default_dt # Time step
-        self.pars = Fish.pars_school
+        self.pars = dict(Fish.default_pars)
 
     @property
     def X(self):
@@ -94,10 +90,10 @@ class Fish(object):
                 # Get a random heading direction
                 heading = np.random.uniform(low=0, high=2*pi)
                 # Add a fish with zero speed/turning speed
-                self.state[i, :] = [x, y, heading, 0, 0]
+                self.state[i, :] = [x, y, heading, 0, self.pars["U_min"]]
         self.nfish = fish_wanted
 
-    def update(self, method, radius, nearest, debug=False):
+    def update(self, method, radius, nearest, debug=False, grab=None):
         # Extract state variables (Nones are required to prevent dimension squeezing)
         X = self.state[:, :2] # cartesian position
         x = self.state[:, 0, None] # cartesian position x
@@ -126,7 +122,7 @@ class Fish(object):
         # Calculate the Voronoi tessellation
         if self.nfish > 3:
             # Construct the Delaunay triangulation
-            tri = Delaunay(X, qhull_options="Qbb Qc Qz Qt")
+            tri = Delaunay(X) # , qhull_options="Qbb Qc Qz Qt") # options needed to make sure all vertices are left in
             # Construct an adjacency matrix from the triangulation
             self.voronoi_adj = np.zeros((self.nfish, self.nfish), dtype=np.bool)
             for simplex in tri.simplices:
@@ -160,34 +156,39 @@ class Fish(object):
                                      x + self.wall_dist*cos_heading) - heading
         self.wall_angle[self.wall_angle < -pi] += 2*pi
 
+        # Mask adjacency matrix for distance
+        adj = self.adj & (self.dist < p["d_c"])
+        
         # Calculate the number of neighbours
-        N_i = self.adj.sum(axis=1)
+        N_i = adj.sum(axis=1)
         N_i_idx = N_i != 0
 
         # Calculate the position angle of adjacent fish relative to the heading angle
-        self.theta_ij = np.arctan2(y_dist[self.adj], x_dist[self.adj]) - np.repeat(heading, N_i) # only bother calculating for adjacent fish for speed
+        self.theta_ij = np.arctan2(y_dist[adj], x_dist[adj]) - np.repeat(heading, N_i) # only bother calculating for adjacent fish for speed
 
         # Helpers
         cos_theta = np.cos(self.theta_ij)
         sin_theta = np.sin(self.theta_ij)
         phi = heading.T - heading
-        sin_phi = np.sin(phi[self.adj])
+        sin_phi = np.sin(phi[adj])
         
         # Calculate distance cut off function for adjacent fish
-        f_d = 1 - np.exp((self.dist[self.adj] - p["d_c"])/p["delta"])
+        f_d = 1 - np.exp((self.dist[adj] - p["d_c"])/p["delta"])
         f_d[f_d < 0] = 0
 
         # Calculate U_i_star
         U_star = np.zeros((self.nfish, self.nfish))
-        U_star[self.adj] = f_d*p["K_s"]*(self.dist[self.adj] - p["r_u"])*cos_theta
+        U_star[adj] = f_d*p["K_s"]*(self.dist[adj] - p["r_u"])*cos_theta
         U_i_star = U_star.sum(axis=1)
         U_i_star[N_i_idx] /= (N_i[N_i_idx]*p["theta_u"])
 
         # Calculate Omega_i_star
+        U_rep = np.repeat(U, N_i)
         Omega_star = np.zeros((self.nfish, self.nfish))
-        Omega_star[self.adj] = f_d*(1 + cos_theta)*(p["K_p"]*(self.dist[self.adj] - p["r_omega"])*sin_theta + p["K_v"]*sin_phi)
+        Omega_star[adj] = f_d*(1 + cos_theta)*(p["K_p"]*(self.dist[adj] - p["r_omega"])*sin_theta/U_rep
+                                                    + U_rep/p["mu_u"]*p["K_v"]*sin_phi/p["theta_omega"])
         Omega_i_star = Omega_star.sum(axis=1)
-        Omega_i_star[N_i_idx] /= (N_i[N_i_idx]*p["theta_omega"])
+        Omega_i_star[N_i_idx] /= N_i[N_i_idx]
         
         # Calculate wall avoidance
         f_W = np.sign(self.wall_angle)*p["f_W_a"]*np.exp(-p["f_W_b"]*self.wall_dist)
@@ -196,27 +197,41 @@ class Fish(object):
         dU_dt = -p["theta_u"]*(U - p["mu_u"] - U_i_star.reshape((self.nfish, 1)))
         dOmega_dt = -p["theta_omega"]*(Omega + f_W - Omega_i_star.reshape((self.nfish, 1)))
 
+        # Coupling of the stochastic terms
+        f_c = p["f_c_a"]*np.exp(-p["f_c_b"]*U);
+        
         # Euler-Maruyama
         U_new = U + dU_dt*self.dt + np.random.randn(self.nfish, 1)*p["sigma_u"]*np.sqrt(self.dt)
-        Omega_new = Omega + dOmega_dt*self.dt + np.random.randn(self.nfish, 1)*p["sigma_0"]*np.sqrt(self.dt)
+        Omega_new = Omega + dOmega_dt*self.dt + np.random.randn(self.nfish, 1)*f_c*np.sqrt(self.dt)
         Omega_new = Omega_new
+
+        # Check for bounds on speeds/turning speeds
+        U_new[U_new < p["U_min"]] = p["U_min"]
+        U_new[U_new > p["U_max"]] = p["U_max"]
+        Omega_new[Omega_new < -p["Omega_max"]] = -p["Omega_max"]
+        Omega_new[Omega_new > p["Omega_max"]] = p["Omega_max"]
 
         # Plain Euler
         heading_new = np.mod(heading + Omega*self.dt, 2*pi)
         x_new = x + U*cos_heading*self.dt
         y_new = y + U*sin_heading*self.dt
 
-        # Check for negative speeds
-        U_new[U_new < 0] = 0
-        
         # Check if any outside the tank
         r2_new = x_new**2 + y_new**2
         idx = r2_new > self.tank_radius**2
         x_new[idx] = x[idx]
         y_new[idx] = y[idx]
-        U_new[idx] = 0
+        U_new[idx] = p["U_min"]
 
-        if debug and has_ipython:
+        # Apply any grabs
+        if grab is not None:
+            x_new[grab, 0] = x[grab, 0]
+            y_new[grab, 0] = y[grab, 0]
+            heading_new[grab, 0] = heading[grab, 0]
+            Omega_new[grab, 0] = Omega[grab, 0]
+            U_new[grab, 0] = U[grab, 0]
+        
+        if debug:
             IPython.embed()
         
         # Update the state
@@ -238,25 +253,37 @@ class GUIHandler(object):
                        "nearest_rad", "nearest_range",
                        "voronoi_rad",
                        "interaction_area",
-                       "sim_speed_range",
-                       "fish_speed_range",
                        "show_interactions_chk",
                        "show_voronoi_chk",
                        "debug_update_chk",
-                       "fish_area"]:
+                       "fish_area",
+                       "speed_range",
+                       "speed_interaction_range",
+                       "speed_rand_range",
+                       "turning_interaction_pos_range",
+                       "turning_interaction_align_range",
+                       "turning_rand_range",
+                       "debug_btn"]:
             setattr(self, widget, builder.get_object(widget))
 
         # Timers to maintain interactivity
-        self.response_time = 100 # milliseconds
-        self.interaction_area_timer = None
+        self.pars_update_time = 100 # milliseconds
+        self.pars_update_timer = None
         self.draw_time = 50 # milliseconds
         self.draw_timer = GObject.timeout_add(self.draw_time, self.update_fish)
 
         # Create some fish
         self.fish = Fish()
 
+        # Update the parameters shown in the GUI
+        self.on_reset_parameters_btn_clicked(None)
+       
         # Scaling for plotting
         self.fish_plotting_scale = 1
+
+        # For grabbing a fish
+        self.grab = None
+        self.cr_matrix = None
         
         # Attach the signals to this object
         builder.connect_signals(self)
@@ -264,14 +291,16 @@ class GUIHandler(object):
         # Show the main window
         self.zebrafish_demo_window.show_all()
 
+        # Remove debugging buttons if necessary
+        if not DEBUG:
+            self.debug_btn.set_visible(False)
+            self.debug_update_chk.set_visible(False)
+
     def update_fish(self):
         # Check that the number of fish hasn't changed
         nfish = int(self.num_fish_range.get_value())
         if self.fish.nfish != nfish:
             self.fish.num_fish_update(nfish)
-        # Update the values of the parameters
-        self.fish.dt = Fish.default_dt*self.sim_speed_range.get_value()
-        self.fish.pars["mu_u"] = self.fish_speed_range.get_value()
         # Update the fish simulation
         if self.radius_rad.get_active():
             method = Fish.radius
@@ -282,7 +311,8 @@ class GUIHandler(object):
         self.fish.update(method,
                          radius=self.radius_range.get_value(),
                          nearest=round(self.nearest_range.get_value()),
-                         debug=self.debug_update_chk.get_active())
+                         debug=self.debug_update_chk.get_active(),
+                         grab=self.grab)
         if self.debug_update_chk.get_active():
             self.debug_update_chk.set_active(False)
         # Draw the fish
@@ -309,6 +339,8 @@ class GUIHandler(object):
         cr.set_source_rgb(0.4, 0.4, 0.4)
         cr.translate(width/2, height/2)
         cr.scale(scale, -scale)
+        self.cr_matrix = cr.get_matrix()
+        self.cr_matrix.invert()
         
         # Draw the tank
         cr.arc(0, 0, self.fish.tank_radius + 12/scale, 0, 2*pi)
@@ -368,16 +400,65 @@ class GUIHandler(object):
             # Restore the transformation matrix
             cr.restore()
 
-
     def on_zebrafish_demo_window_delete_event(self, *args):
         # Window has been closed - follow suit
         Gtk.main_quit(*args)
 
     def on_debug_btn_clicked(self, button):
-        if has_ipython:
-            IPython.embed()
-        
-        
+        IPython.embed()
+
+    def on_par_value_changed(self, range):
+        # Put updates on a timer to avoid changing them thousands of times a second
+        if self.pars_update_timer is None:
+            self.pars_update_timer = GObject.timeout_add(self.pars_update_time, self.update_pars)
+
+    def update_pars(self):
+        self.pars_update_timer = None
+        self.fish.pars["mu_u"] = self.speed_range.get_value()
+        self.fish.pars["K_s"] = self.speed_interaction_range.get_value()
+        self.fish.pars["sigma_u"] = self.speed_rand_range.get_value()
+        self.fish.pars["K_p"] = self.turning_interaction_pos_range.get_value()
+        self.fish.pars["K_v"] = self.turning_interaction_align_range.get_value()
+        self.fish.pars["f_c_a"] = self.turning_rand_range.get_value()
+        return False
+
+    def on_reset_positions_btn_clicked(self, button):
+        # Reset the fish positions
+        nfish = self.fish.nfish
+        self.fish.num_fish_update(0)
+        self.fish.num_fish_update(nfish)
+
+    def on_reset_parameters_btn_clicked(self, button):
+        # Set the default parameters in the GUI
+        self.speed_range.set_value(Fish.default_pars["mu_u"])
+        self.speed_interaction_range.set_value(Fish.default_pars["K_s"])
+        self.speed_rand_range.set_value(Fish.default_pars["sigma_u"])
+        self.turning_interaction_pos_range.set_value(Fish.default_pars["K_p"])
+        self.turning_interaction_align_range.set_value(Fish.default_pars["K_v"])
+        self.turning_rand_range.set_value(Fish.default_pars["f_c_a"])
+        self.fish.pars = dict(Fish.default_pars)
+
+    def on_fish_area_button_press_event(self, widget, event):
+        if self.cr_matrix is not None:
+            pt = self.cr_matrix.transform_point(event.x, event.y)
+            self.grab = np.argmin((self.fish.x - pt[0])**2 + (self.fish.y - pt[1])**2)
+
+    def on_fish_area_button_release_event(self, widget, event):
+        self.grab = None
+
+    def on_fish_area_motion_notify_event(self, widget, event):
+        if self.grab is not None:
+            if self.cr_matrix is not None:
+                pt = self.cr_matrix.transform_point(event.x, event.y)
+                dist = np.sqrt(pt[0]**2 + pt[1]**2)
+                if dist > (self.fish.tank_radius - 0.1):
+                    angle = np.arctan2(pt[1], pt[0])
+                    dist = self.fish.tank_radius - 0.1
+                    pt = (dist*cos(angle), dist*sin(angle))
+                self.fish.state[self.grab, 0] = pt[0]
+                self.fish.state[self.grab, 1] = pt[1]
+
+    
 # Work out where the Python file is
 py_path = os.path.dirname(os.path.realpath(__file__))
                 
